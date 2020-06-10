705f8efc52c98a1a7f99ad1fc3c123dfe5382b88
Asaf <casaf5@gmail.com>
2020-06-10 14:23:57 +0300

    fixed focus and adding new text

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var gCtx
 var gIsClicked = false;
 var gLastPoint = { x: 0, y: 0 }
 var gKeywords = { 'happy': 12, 'funny puk': 1 }
-var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
+var gImgs = [{ id: 5, url: './memes/5.jpg', keywords: ['happy'] }, { id: 10, url: './memes/10.jpg', keywords: ['happy'] }];
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
@@ -35,86 +35,61 @@ var gSettings = {
 }
 function init() {
     setCanvas()
-    document.addEventListener('mousedown', (ev) => {
-        gIsClicked = true
-        gLastPoint.x = ev.offsetX
-        gLastPoint.y = ev.offsetY
-    })
-    document.addEventListener('mouseup', () => {
-        gIsClicked = false
-    })
-    drawImg2()
+    drawMeme()
 }
-
-
 function setCanvas() {
     gElCanvas = document.getElementById('my-canvas')
     gCtx = gElCanvas.getContext('2d')
 }
 
-function startDraw(ev) {
-    const { offsetX: x, offsetY: y } = ev
-    gCtx.strokeStyle = gSettings.color
-    gCtx.fillStyle = gSettings.color
-    gCtx.globalAlpha = gSettings.opacity / 100
-    gCtx.beginPath()
-    if (gIsClicked) {
-        switch (gSettings.shape) {
-            case 'line':
-                drawLine(x, y)
-                break;
-            case 'rect':
-                drawRect(x, y)
-                break;
-            case 'circle':
-                drawCircle(x, y)
-        }
-        gLastPoint.x = x
-        gLastPoint.y = y
-    }
+function onEditNewTxt(elTxt) {
+    var focusedLine = getLine(gMeme.selectedLineIdx)
+    focusedLine.txt = elTxt.value
+    gCtx.font = `${focusedLine.size}px Impact`
+    gCtx.align = `${focusedLine.align}`
+    gCtx.textBaseline = "top"
+    gCtx.fillText(focusedLine.txt, focusedLine.posX, focusedLine.posY,
+        gCtx.measureText(focusedLine.txt).width, focusedLine.size)
+    drawMeme()
+    focusOnText(focusedLine)
 
 }
-function drawLine(x, y) {
-    gCtx.moveTo(gLastPoint.x, gLastPoint.y)
-    gCtx.lineTo(x, y)
-    gCtx.closePath()
-    gCtx.stroke()
-}
 
-function drawTxt() {
-    var userTxt = document.getElementById('user-txt').value
+function addNewTxt() {
     gMeme.lines.push({
         id: gMeme.lines.length,
-        txt: userTxt,
-        size: 40, align: 'center',
+        txt: 'Enter New Text',
+        size: 30, align: 'center',
         color: 'black',
-        posX: 250,
-        posY: 250,
+        posX: gElCanvas.width / 2,
+        posY: gElCanvas.height / 2,
     })
+    gMeme.selectedLineIdx = gMeme.lines.length - 1
     drawMeme()
+    
 }
 
 function setCanvasMeme(elImg) {
-    gCtx.drawImage(elImg, 0, 0, 500, 500);
-    gMeme.selectedImgId = elImg.id
+    gMeme.selectedImgId = +elImg.id
     drawMeme()
 }
-function drawImg2() {
+function setImage(imgId) {
+    var img = gImgs.find(img => img.id === imgId)
     var elImg = new Image();
-    elImg.src = './memes/1.jpg';
-    elImg.onload = () => {
-        gCtx.drawImage(elImg, 0, 0, 500, 500);
-    }
+    elImg.src = img.url;
+    gCtx.drawImage(elImg, 0, 0, 500, 500);     //!Add correct aspect ratio
 }
 
 function drawMeme() {
+    setImage(gMeme.selectedImgId)
     gMeme.lines.forEach(line => {
         gCtx.font = `${line.size}px Impact`
         gCtx.fillStyle = line.color
         gCtx.textAlign = line.align
+        gCtx.textBaseline = "top"
         gCtx.fillText(line.txt, line.posX, line.posY)
     })
-
+    focusOnText(getLine(gMeme.selectedLineIdx))
 }
 
 function onChangeFontSize(diff) {
@@ -127,7 +102,18 @@ function onChangeFontSize(diff) {
 function onChangeTextFocus() {
     gMeme.selectedLineIdx++
     if (gMeme.selectedLineIdx >= gMeme.lines.length) gMeme.selectedLineIdx = 0
+    drawMeme()
 }
+
+function focusOnText(focusedLine) {
+    var currWidth = gCtx.measureText(focusedLine.txt).width
+    gCtx.beginPath()
+    gCtx.rect(focusedLine.posX - currWidth / 2, focusedLine.posY, currWidth, focusedLine.size)
+    gCtx.stroke()
+}
+
+
+
 
 function getLine(lineId) {
     return gMeme.lines.find(line => line.id === lineId)
@@ -143,3 +129,4 @@ function onChangeLineLocation(diff, posToChange) {
     gCtx.clearRect(0, 0, 500, 500)
     drawMeme()
 }
+
